324f575e03e2f9477707519ff55f446eadee9e39
Christopher-Gustavsson <chrisgustavsson1@gmail.com>
2019-03-22 15:44:16 -0700

    added create student endpoint

--- dummydata/studentlist.js
+++ dummydata/studentlist.js
@@ -7,3 +7,9 @@ var list = {
 		{'id': 4, 'name': 'Robert', 'course': 'Linear Algebra', 'grade': 1}
 	]
 }
+
+//SELECT `id`, CONCAT(`givenname`, " ",  `surname`) AS `name`, `course`, `grade` FROM `grades`   - this is the phpmyadmin query
+
+// INSERT INTO `grades` SET `surname`="Chris", `givenname`="Gustavsson", `course`="math", `grade`=80, `added`=NOW() -- this can insert 1
+
+//INSERT INTO `grades` (`surname`, `givenname`, `course`, `grade`) VALUES ("Gustavsson", "Christopher", "math", 80), ("postman", "postman", "math", 80) this can insert many
